7c63ba4f3566dd8f39830461e4ad21eca41f47f0
王皓雯 <3178243510@qq.com>
2023-07-20 16:45:40 +0800

    修复倒计时的bug
    修复了正在计时且重新设置倒计时时间的时候出现的bug

--- js/countdown_control.js
+++ js/countdown_control.js
@@ -55,17 +55,23 @@ function drawNumerialTime() {
 }
 
 
+let timer_id;
 settime_btn.addEventListener('click',()=>{
     var time = prompt('请输入时间',clock.getTimeString()); //点击按钮弹出输入框
     let times = time.split(':').map((x)=>{return Number(x)}) //解析用户输入
     clock.setTime(...times,0) //给clock设置时间
     let origin_tick = clock.global_tick //记录总时间，用于计算剩下时间的比例
     let stop = false;  //倒计时是否停止？
-    let timer_id = setInterval(()=>{  //保留id便于停止倒计时
+    if(timer_id!=undefined) {
+        clearInterval(timer_id)
+        timer_id=undefined
+    }
+    timer_id = setInterval(()=>{  //保留id便于停止倒计时
         drawClock(origin_tick)
         drawNumerialTime()
         if(stop) { //如果倒计时停止
-            clearInterval(timer_id) 
+            clearInterval(timer_id)
+            timer_id=undefined
             setTimeout(()=>{   //延迟50ms弹出提示框，原因在于防止浏览器渲染不及时，导致环形条无法正常清零
                 alert("时间到了！")  
             },50)
