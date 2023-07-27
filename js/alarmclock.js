9d03c501e9be0cb5fa08fdbd741744360a3454a9
leishaforlinminzhi <15611664060@qq.com>
2023-07-27 19:37:46 +0800

    修改了输出

--- js/alarmclock.js
+++ js/alarmclock.js
@@ -24,25 +24,25 @@ function showLocalTime() {
 
     switch(day){
         case 1:
-            day='一';
+            day='星期一';
             break;
         case 2:
-            day='二';
+            day='星期二';
             break;
         case 3:
-            day='三';
+            day='星期三';
             break;
         case 4:
-            day='四';
+            day='星期四';
             break;
         case 5:
-            day='五';
+            day='星期五';
             break;
         case 6:
-            day='六';
+            day='星期六';
             break;
         case 7:
-            day='日';
+            day='星期日';
             break;
         default:
             break;
